26d7af941df4a532eaa94a5b99cd22f7f4b3e737
kudo <Gokus12456@gmail.com>
2026-05-19 09:11:04 +0530

    Add null check for accessory name
    Add null check for the 'name' variable before processing.

--- services/core/java/com/android/server/WiredAccessoryManager.java
+++ services/core/java/com/android/server/WiredAccessoryManager.java
@@ -556,6 +556,9 @@ final class WiredAccessoryManager implements WiredAccessoryCallbacks {
 
             if (name == null) {
                 name = event.get("SWITCH_NAME");
+                if(name == null) {
+                    return;
+                }
             }
 
             try {
