crdroidandroid/android_frameworks_base

SystemUI: Add toggle for translucent notifications on lockscreen
Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ActivatableNotificationView.java
@@ -120,6 +120,7 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
     private boolean mDismissed;
     private boolean mRefocusOnDismiss;
     private boolean mTranslucencyEnabled;
+    protected boolean mLockscreenTranslucencyEnabled;
     private boolean mUseTranslucency;
 
     public ActivatableNotificationView(Context context, AttributeSet attrs) {
@@ -343,6 +344,13 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
     public void setIsTranslucencyEnabled(boolean isTranslucencyEnabled) {
         if (mTranslucencyEnabled != isTranslucencyEnabled) {
             mTranslucencyEnabled = isTranslucencyEnabled;
+            updateIfNeeded();
+        }
+    }
+
+    public void setIsLockscreenTranslucencyEnabled(boolean isTranslucencyEnabled) {
+        if (mLockscreenTranslucencyEnabled != isTranslucencyEnabled) {
+            mLockscreenTranslucencyEnabled = isTranslucencyEnabled;
             updateIfNeeded();
         }
     }

packages/SystemUI/src/com/android/systemui/window/data/repository/NoopWindowRootViewBlurRepository.kt
@@ -25,5 +25,6 @@ class NoopWindowRootViewBlurRepository @Inject constructor() : WindowRootViewBlu
     override val isBlurOpaque: MutableStateFlow<Boolean> = MutableStateFlow(true)
     override val isBlurSupported: StateFlow<Boolean> = MutableStateFlow(false)
     override val isTranslucentEnabled: StateFlow<Boolean> = MutableStateFlow(false)
+    override val isLockscreenTranslucentEnabled: StateFlow<Boolean> = MutableStateFlow(false)
     override var blurAppliedListener: BlurAppliedListener? = null
 }

shinichi-c/frameworks_base_evoX

packages/SystemUI/src/com/android/systemui/statusbar/notification/row/ActivatableNotificationView.java
@@ -129,6 +129,7 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
      */
     protected boolean mOnKeyguard;
     protected boolean mIsBlurSupported;
+    protected boolean mIsLockscreenBlurSupported;
     protected boolean mUseTransparent;
 
     public ActivatableNotificationView(Context context, AttributeSet attrs) {
@@ -351,6 +352,11 @@ public abstract class ActivatableNotificationView extends ExpandableOutlineView 
 
     public void setIsBlurSupported(boolean isBlurSupported) {
         mIsBlurSupported = isBlurSupported;
+        updateIfNeeded();
+    }
+
+    public void setIsLockscreenBlurSupported(boolean isBlurSupported) {
+        mIsLockscreenBlurSupported = isBlurSupported;
         updateIfNeeded();
     }
 

packages/SystemUI/src/com/android/systemui/window/data/repository/NoopWindowRootViewBlurRepository.kt
@@ -25,6 +25,7 @@ class NoopWindowRootViewBlurRepository @Inject constructor() : WindowRootViewBlu
     override val scaleRequestedByShade: MutableStateFlow<Float> = MutableStateFlow(1.0f)
     override val isBlurSupported: StateFlow<Boolean> = MutableStateFlow(false)
     override val isTranslucentSupported: StateFlow<Boolean> = MutableStateFlow(false)
+    override val isLockscreenTranslucentSupported: StateFlow<Boolean> = MutableStateFlow(false)
     override var blurAppliedListener: BlurAppliedListener? = null
     override val trackingShadeMotion: MutableStateFlow<Boolean> = MutableStateFlow(false)
 }